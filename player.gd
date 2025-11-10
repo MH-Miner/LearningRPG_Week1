09aa5f7a35c334188f1bdb4fdb521c292f5fceb7
MH-Miner <sminer2@schools.nyc.gov>
2025-11-09 23:00:40 -0500

    stripped down for easy use

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print($Label.text + "Player is ready!")
+	pass
 
 # Called every physics frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
